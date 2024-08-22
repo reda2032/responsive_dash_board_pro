35ce3ac1646d039a7f9557504a4b690bc371e4c4
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-08-22 21:09:10 +0300

    Hide appbar

--- lib/theory_to_practice/views/home_view.dart
+++ lib/theory_to_practice/views/home_view.dart
@@ -19,19 +19,54 @@ class _HomeViewState extends State<HomeView> {
       key: scaffoldKey,
       drawer: const CustomDrawer(),
       backgroundColor: const Color(0xffDBDBDB),
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        leading: GestureDetector(
-          onTap: () {
-            scaffoldKey.currentState?.openDrawer();
-          },
-          child: const Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-        ),
-      ),
+      appBar: MediaQuery.sizeOf(context).width < 900
+          ? AppBar(
+              backgroundColor: Colors.black,
+              leading: GestureDetector(
+                onTap: () {
+                  scaffoldKey.currentState?.openDrawer();
+                },
+                child: const Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                ),
+              ),
+            )
+          : null,
       body: const HomeViewBody(),
     );
   }
 }
+
+/*
+class CustomAdaptiveAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  const CustomAdaptiveAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth < 900) {
+        return AppBar(
+          backgroundColor: Colors.black,
+          leading: GestureDetector(
+            onTap: () {
+              //  scaffoldKey.currentState?.openDrawer();
+            },
+            child: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+          ),
+        );
+      } else {
+        return const SizedBox();
+      }
+    });
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(65.0);
+}
+*/
