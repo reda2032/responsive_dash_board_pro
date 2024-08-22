512dac5ef887cea017337252058fac6e895b8a8a
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-08-22 13:42:41 +0300

    Drawer Header

--- lib/theory_to_practice/views/widgets/custom_drawer.dart
+++ lib/theory_to_practice/views/widgets/custom_drawer.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Drawer(
+      backgroundColor: Color(0xffDBDBDB),
+      child: Column(
+        children: [
+          DrawerHeader(
+            child: Icon(
+              FontAwesomeIcons.solidHeart,
+              size: 48,
+            ),
+          ),
+          ListTile(
+            title: Text('Item 1'),
+          ),
+          ListTile(
+            title: Text('Item 2'),
+          ),
+        ],
+      ),
+    );
   }
 }
