d11c66db1d16e9f61ee209399cf65e28df6707b8
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-08-22 20:18:50 +0300

    Desktop layout part two

--- lib/theory_to_practice/views/widgets/custom_item1.dart
+++ lib/theory_to_practice/views/widgets/custom_item1.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class CustomItem1 extends StatelessWidget {
-  const CustomItem1({super.key});
-
+  const CustomItem1({super.key, this.color});
+  final Color? color;
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        color: Color(0xffB4B4B4),
-        borderRadius: BorderRadius.all(
+      decoration: BoxDecoration(
+        color: color ?? const Color(0xffB4B4B4),
+        borderRadius: const BorderRadius.all(
           Radius.circular(8),
         ),
       ),
--- lib/theory_to_practice/views/widgets/desktop_layout.dart
+++ lib/theory_to_practice/views/widgets/desktop_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/theory_to_practice/views/widgets/tablet_layout.dart';
 
+import 'custom_desktop_widgets.dart';
 import 'custom_drawer.dart';
 
 class DesktopLayout extends StatelessWidget {
@@ -11,8 +12,9 @@ class DesktopLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Row(
       children: [
-        CustomDrawer(),
-        Expanded(child: TabletLayout()),
+        Expanded(child: CustomDrawer()),
+        Expanded(flex: 3, child: TabletLayout()),
+        Expanded(child: CustomDesktopWidgets()),
       ],
     );
   }
