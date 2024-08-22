c96d1b4539339110ae3f6bdf1fa027e9c9a45931
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-08-22 11:07:57 +0300

    Custom Sliver List

---
+++ lib/theory_to_practice/views/widgets/custom_sliver_list_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'custom_item2.dart';
+
+class CustomSliverListView extends StatelessWidget {
+  const CustomSliverListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList.builder(
+        itemCount: 15,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.only(top: 16),
+            child: CustomItem2(),
+          );
+        });
+  }
+}
--- lib/theory_to_practice/views/widgets/home_view_body.dart
+++ lib/theory_to_practice/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'custom_sliver_grid.dart';
+import 'custom_sliver_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -16,6 +17,8 @@ class HomeViewBody extends StatelessWidget {
           ),
           // custom_sliver_grid
           CustomSliverGrid(),
+          // CustomListView
+          CustomSliverListView(),
         ],
       ),
     );
