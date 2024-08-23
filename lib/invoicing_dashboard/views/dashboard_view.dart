57c44f7720a04920b00d57f95b2c5d2da62da46e
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-08-23 18:01:36 +0300

    Create dashboard desktop layout

--- lib/invoicing_dashboard/views/dashboard_view.dart
+++ lib/invoicing_dashboard/views/dashboard_view.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/adaptive_layout.dart';
+import '../widgets/dashboard_desktop_layout.dart';
+
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobilLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DashboardDesktopLayout(),
+      ),
+    );
   }
 }
