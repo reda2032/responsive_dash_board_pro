504347bd84012bbf68e0595311f8626eea94eccc
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-08-24 14:43:48 +0300

    user info list tile part

--- lib/invoicing_dashboard/widgets/dashboard_desktop_layout.dart
+++ lib/invoicing_dashboard/widgets/dashboard_desktop_layout.dart
@@ -1,10 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'custom_drawer.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Row(
+      children: [
+        Expanded(child: CustomDrawer()),
+      ],
+    );
   }
 }
---
+++ lib/invoicing_dashboard/widgets/user_info_list_tile.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../utils/app_styles.dart';
+
+class UserInfoListTile extends StatelessWidget {
+  const UserInfoListTile({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subtitle,
+  });
+
+  final String image, title, subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color(0xFFFAFAFA),
+      elevation: 0,
+      child: Center(
+        child: ListTile(
+          leading: SvgPicture.asset(image),
+          title: Text(
+            title,
+            style: AppStyles.styleSemiBold16(context),
+          ),
+          subtitle: Text(
+            subtitle,
+            style: AppStyles.styleRegular12(context),
+          ),
+        ),
+      ),
+    );
+  }
+}
