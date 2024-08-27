253bba3e308b8ea336b7168788b439d6a22cde36
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-08-27 22:42:09 +0300

    All expenses header

--- lib/dashboard/widgets/all_expenses.dart
+++ lib/dashboard/widgets/all_expenses.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/dashboard/widgets/all_expensess_header.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      decoration: ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          )),
+      child: const Column(
+        children: [
+          AllExpensessHeader(),
+        ],
+      ),
+    );
   }
 }
